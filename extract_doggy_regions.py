d3ccf343ba6064b447a5f54a9bd71ab4c90f51d9
christian-desktop <portlyflounder.dev@gmail.com>
2022-04-25 17:41:03 +0200

    update extract regions script to process json file.

--- extract_doggy_regions.py
+++ extract_doggy_regions.py
@@ -1,27 +1,30 @@
 """A python script that extracts annotated dog regions from images."""
 
 import cv2
+import json
 import numpy as np
 from pathlib import Path
 
-ANNOTATED_IMAGE_DIR = "data/annotations"
-RAW_IMAGE_DIR = "data/raw"
+JSON_FILE = "reviewed_labels.json"
+ANNOTATED_REGIONS_OUTPUT_DIR = "data/annotated_regions"
 
 def run():
-    for anno_image_filepath in Path(ANNOTATED_IMAGE_DIR).iterdir():
-        anno_image_name = anno_image_filepath.name
-        raw_image_filepath = Path(RAW_IMAGE_DIR).joinpath(anno_image_name).with_suffix(".jpg")
+    with open(JSON_FILE, "r") as json_file:
+        reviewed_image_data = json.loads(json_file.read())
+    for metadata in reviewed_image_data.values():
+        acceptable_annotation = metadata["acceptable"]
+        if acceptable_annotation:
+            filepaths = metadata["filepaths"]
+            anno_image_filepath = filepaths["annotation"]
+            raw_image_filepath = filepaths["raw"]
 
-        if not raw_image_filepath.exists():
-            raise FileNotFoundError(f"The raw image '{raw_image_filepath}' does not exist for the annotated image {anno_image_filepath}.")
-        
-        anno_image_gray = cv2.imread(str(anno_image_filepath), cv2.IMREAD_GRAYSCALE)
-        raw_image = cv2.imread(str(raw_image_filepath))
+            anno_image_gray = cv2.imread(str(anno_image_filepath), cv2.IMREAD_GRAYSCALE)
+            raw_image = cv2.imread(str(raw_image_filepath))
 
-        annotated_region_mask = (anno_image_gray < 255).astype(np.uint8)
-        annotated_region = raw_image * np.stack([annotated_region_mask, annotated_region_mask, annotated_region_mask], axis=-1)
-        
-        
+            annotated_region_mask = (anno_image_gray < 255).astype(np.uint8)
+            annotated_region = raw_image * np.stack([annotated_region_mask, annotated_region_mask, annotated_region_mask], axis=-1)
+            annotated_region_filepath = Path(ANNOTATED_REGIONS_OUTPUT_DIR).joinpath(Path(anno_image_filepath).name).with_suffix(".jpg")
+            cv2.imwrite(str(annotated_region_filepath), annotated_region)
     
 if __name__ == "__main__":
     run()
